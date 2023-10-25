cb0349f1ef8a31f5e01a5ef44cf47de098038911
sagazirobiul <sagazirobiul@gmail.com>
2023-10-25 17:10:23 +0600

    change the loging url

--- js/login.js
+++ js/login.js
@@ -3,7 +3,7 @@ const pass = document.getElementById('pass');
 
 document.getElementById('submit-btn').addEventListener('click', function(){
     if(email.value === 'baper@bank.com' && pass.value === 'secret'){
-        window.location.href = 'https://sagazirobiul.github.io/baper-bank/inside-bank.html';
+        window.location.href = 'https://sagazirobiul.github.io/quezed-bank/inside-bank.html';
     } else if(email.value === ''){
         alert('Please write your email :)')
     } 
