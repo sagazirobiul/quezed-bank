c7e95f8040ae8ffa70f93a47357b2354a9907997
sagazirobiul <sagazirobiul@gmail.com>
2023-10-25 16:33:07 +0600

    updated loign redirect url

--- js/login.js
+++ js/login.js
@@ -3,7 +3,7 @@ const pass = document.getElementById('pass');
 
 document.getElementById('submit-btn').addEventListener('click', function(){
     if(email.value === 'baper@bank.com' && pass.value === 'secret'){
-        window.location.href = 'http://127.0.0.1:5500/bank.html';
+        window.location.href = 'https://sagazirobiul.github.io/baper-bank/funtional-baper-bank.html';
     } else if(email.value === ''){
         alert('Please write your email')
     } 
